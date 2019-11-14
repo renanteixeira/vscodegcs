098b18af5202b229e320425f77e7cc8d1c20d843
Renan Teixeira <git@renanteixeira.com.br>
2019-11-13 22:42:50 -0300

    Create codeserver.sh

---
+++ codeserver.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+###################################################################
+#Script Name	: codeserver.sh
+#Description	: Run the latest version of Visual Studio Code on Google Cloud Shell
+#Author       	: Renan Teixeira
+#Email         	: contato@renanteixeira.com.br
+#Version        : 0.1 - 13/11/2019 - 22:40
+###################################################################
+
+echo 'Run Visual Studio Code'
+
+export VERSION=`curl -s https://api.github.com/repos/cdr/code-server/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+
+codeRoot="$HOME/codeserver"
+
+downloadCodeServer(){
+    cd $codeRoot
+    echo "Code Server Version $VERSION not found, downloading...."
+    wget -c https://github.com/cdr/code-server/releases/download/$VERSION/code-server$VERSION-linux-x86_64.tar.gz -O $VERSION.tar.gz
+
+    echo "Download completed, unzipping file..."
+    mkdir $VERSION && tar xvzf $VERSION.tar.gz -C $VERSION --strip-components 1
+    
+    echo "Unpacking completed..."
+    rm -f $VERSION.tar.gz
+    
+    cd $codeRoot/$VERSION
+    ./code-server --no-auth --port 8080
+}
+
+runCodeServer(){
+    if [ -d "$codeRoot/$VERSION" ]; then
+        echo "Code Server Version $VERSION exists"
+        echo "Running..."
+    
+        cd $codeRoot/$VERSION
+        ./code-server --no-auth --port 8080
+    else
+        downloadCodeServer
+    fi
+}
+
+if [ ! -d "$codeRoot" ]; then
+    mkdir $codeRoot
+    downloadCodeServer
+else
+    runCodeServer
+fi
