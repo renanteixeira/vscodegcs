94f2d9f58e322950771ddb810d5c1f74b586baa5
Renan Teixeira <git@renanteixeira.com.br>
2020-07-31 02:56:46 -0300

    Code Server 3.4.x and Improvements

--- README.md
+++ README.md
@@ -8,29 +8,36 @@ I saw this Idea in a [Christiaan Hees](https://medium.com/google-cloud/how-to-ru
 
 This script runs the latest version of [code-server (VS Code)](https://github.com/cdr/code-server) with just one command.
 
-This script needs the following packages:
-
-- [jq](https://stedolan.github.io/jq/)
-
-### via curl
+## via curl
 ```bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/renanteixeira/vscodegcs/master/codeserver.sh)"
 ```
 
-### via wget
+## via wget
 ```bash
 sh -c "$(wget https://raw.githubusercontent.com/renanteixeira/vscodegcs/master/codeserver.sh -O -)"
 ```
 
-## After running
+## Running
 
-After executing the above command, you will see the information below.
+> First run, you will have to select the settings.
+This screen will create a configuration file in "~/ .config/code-server/config.yaml"
 
+![bindAddr](https://user-images.githubusercontent.com/6026211/89003094-378bd780-d2d5-11ea-90c3-ac9e38efcc8a.png)
+![authMode](https://user-images.githubusercontent.com/6026211/89003167-66a24900-d2d5-11ea-9615-425d6b93652d.png)
+
+> New executions, you will be asked if you want to use the settings already saved or if you want to create new ones.
+
+![config](https://user-images.githubusercontent.com/6026211/89003709-e381f280-d2d6-11ea-8e9a-570cc3b963c0.png)
 
 ```bash
-info  Server listening on http://localhost:8080
-info    - Password is ## A random password is generated.
-info      - To use your own password, set the PASSWORD environment variable
+password: (Random string or Custom Password)
+
+info  Using config file ~/.config/code-server/config.yaml
+info  Using user-data-dir ~/.local/share/code-server
+info  code-server 3.4.1 48f7c2724827e526eeaa6c2c151c520f48a61259
+info  HTTP server listening on http://127.0.0.1:8080
+info      - Using password from ~/.config/code-server/config.yaml
 info      - To disable use `--auth none`
 info    - Not serving HTTPS
 ```
@@ -48,8 +55,11 @@ I recommend using [Boost Mode in Cloud Shell](https://cloud.google.com/shell/doc
 
 ![Boost Mode](https://miro.medium.com/max/2574/1*D8J4okWRgFRnxo7MhbHGPg.png)
 
-### That’s it, now you have an IDE running in your browser!
+## That’s it, now you have an IDE running in your browser!
 ![VS Code Online](https://miro.medium.com/max/1996/1*BVCoNecwFAR7vcmaJ9a2gA.png)
+
+## FAQ Code Server
+See [CDR FAQ](https://github.com/cdr/code-server/blob/master/doc/guide.md).
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- codeserver.sh
+++ codeserver.sh
@@ -1,70 +1,321 @@
 #!/bin/bash
+set -eu
+
 ###################################################################
 #Script Name	: codeserver.sh
-#Description	: Run the latest version of Visual Studio Code on Google Cloud Shell
+#Description	: Run the latest version of Code Server (Visual Studio Code) in the browser.
 #Author       	: Renan Teixeira
 #Email         	: contato@renanteixeira.com.br
-#Version        : 0.4 - 19/01/2020 - 12:40
+#Version        : 0.5 - 03/07/2020 - 21:50
 ###################################################################
 
-echo 'Run Visual Studio Code'
+#Global variables
+GREEN="\e[92m"
+RED="\e[31m"
+END="\e[0m"
+BLUE="\e[34m"
+YELLOW="\e[33m"
 
-if dpkg-query -s jq 1>/dev/null 2>&1; then
-    echo
-else
-    echo "Package JQ not found!"
-    echo
-    echo "[1] Install JQ package and run script"
-    echo "[2] Exit script"
-    echo
-    echo -n "Chose an option "
-    read option
-    case $option in
-        1) sudo apt-get install jq -y ;;
-        2) exit ;;
-        *) exit ;;
-    esac
-fi
+echo_latest_version() {
+  version="$(curl -fsSLI -o /dev/null -w "%{url_effective}" https://github.com/cdr/code-server/releases/latest)"
+  version="${version#https://github.com/cdr/code-server/releases/tag/}"
+  version="${version#v}"
+  echo "$version"
+}
 
-export VERSION=`curl -s https://api.github.com/repos/cdr/code-server/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+VERSION="${VERSION-$(echo_latest_version)}"
 
-export NAME=`curl -s https://api.github.com/repos/cdr/code-server/releases/latest |jq .name -r`
+CODE_PATH="$HOME/.config/code-server"
+CONFIG_FILE=$CODE_PATH/config.yaml
+CODE_FILE=$CODE_PATH/$VERSION/bin/code-server
 
-codeRoot="$HOME/codeserver"
+main() {
+	OS="$(os)"
+	if [ ! "$OS" ]; then
+		echoerr "Unsupported OS $(uname)."
+		exit 1
+	fi
+
+	distro_name
+
+	ARCH="$(arch)"
+
+	if [ ! -d "$CODE_PATH" ]; then
+		mkdir $CODE_PATH
+		if [[ -f $CODE_FILE ]]; then
+			downloadCodeServer
+		else
+			runCodeServer
+		fi
+	else
+		clear
+		runCodeServer
+	fi
+}
+
+os() {
+  case "$(uname)" in
+  Linux)
+    echo linux
+    ;;
+  Darwin)
+    echo macos
+    ;;
+  FreeBSD)
+    echo freebsd
+    ;;
+  esac
+}
+
+distro() {
+  if [ "$OS" = "macos" ] || [ "$OS" = "freebsd" ]; then
+    echo "$OS"
+    return
+  fi
+
+  if [ -f /etc/os-release ]; then
+    (
+      . /etc/os-release
+      case "$ID" in opensuse-*)
+        # opensuse's ID's look like opensuse-leap and opensuse-tumbleweed.
+        echo "opensuse"
+        return
+        ;;
+      esac
+
+      echo "$ID"
+    )
+    return
+  fi
+}
+
+# os_name prints a pretty human readable name for the OS/Distro.
+distro_name() {
+  if [ "$(uname)" = "Darwin" ]; then
+    echo "macOS v$(sw_vers -productVersion)"
+    return
+  fi
+
+  if [ -f /etc/os-release ]; then
+    (
+      . /etc/os-release
+      echo "$PRETTY_NAME"
+    )
+    return
+  fi
+
+  # Prints something like: Linux 4.19.0-9-amd64
+  uname -sr
+}
+
+arch() {
+  case "$(uname -m)" in
+  aarch64)
+    echo arm64
+    ;;
+  x86_64)
+    echo amd64
+    ;;
+  amd64) # FreeBSD.
+    echo amd64
+    ;;
+  esac
+}
 
 downloadCodeServer(){
-    cd $codeRoot
-    echo "Code Server Version $VERSION not found, downloading...."
-    wget -c https://github.com/cdr/code-server/releases/download/$VERSION/code-server-$NAME-linux-x86_64.tar.gz -O $VERSION.tar.gz
+	echoh "Downloading standalone release archive v$VERSION from GitHub releases."
+	echoh
 
-    echo "Download completed, unzipping file..."
-    mkdir $VERSION && tar xvzf $VERSION.tar.gz -C $VERSION --strip-components 1
+	cd $CODE_PATH
+	fetch "https://github.com/cdr/code-server/releases/download/v$VERSION/code-server-$VERSION-$OS-$ARCH.tar.gz" \
+    "$VERSION.tar.gz"
+
+    untarCodeServer
+}
+
+untarCodeServer(){
+	cd $CODE_PATH
+	mkdir $VERSION && tar xzf $VERSION.tar.gz -C $VERSION --strip-components 1
     
     echo "Unpacking completed..."
     rm -f $VERSION.tar.gz
-    
-    clear
-    cd $codeRoot/$VERSION
-    ./code-server --port 8080
+
+    configFile
 }
 
 runCodeServer(){
-    if [ -d "$codeRoot/$VERSION" ]; then
-        echo "Code Server Version $VERSION exists"
+    if [[ -f $CODE_FILE ]]; then
+    	printf "${GREEN}"
+        printf "Code Server Version ${BLUE}$VERSION${END} ${GREEN}exists\n"
         echo "Running..."
+        echo
+        printf "${END}"
     
-        cd $codeRoot/$VERSION
-        ./code-server --port 8080
+        configFile
     else
         downloadCodeServer
     fi
 }
 
-if [ ! -d "$codeRoot" ]; then
-    clear
-    mkdir $codeRoot
-    downloadCodeServer
-else
-    clear
-    runCodeServer
-fi
+aboutConfigFile(){
+cath << EOF
+	The configuration file contains the variables that facilitate the parameterization of the Code Server according to its environment.
+
+	This file is located at $CONFIG_FILE if you need to modify it manually.
+
+EOF
+}
+
+rand-str() {
+    # Return random alpha-numeric string of given LENGTH
+    #
+    # Usage: VALUE=$(rand-str $LENGTH)
+    #    or: VALUE=$(rand-str)
+
+    local DEFAULT_LENGTH=64
+    local LENGTH=${1:-$DEFAULT_LENGTH}
+
+    tr -dc A-Za-z0-9 </dev/urandom | head -c $LENGTH
+    # -dc: delete complementary set == delete all except given set
+}
+
+configFile(){
+	aboutConfigFile
+
+	if [[ ! -f $CONFIG_FILE ]]; then
+		printf "${RED}"
+		echo "Configuration file does not exist, let's create!"
+		printf "${END}"
+
+		newConfigFile
+	elif [[ ! -s $CONFIG_FILE ]]; then
+		printf "${RED}"
+		echo "Configuration file is empty!"
+		printf "${END}"
+
+		newConfigFile
+	else
+		printf "${YELLOW}"
+		echo "Use existing settings or create new ones?"
+		printf "${END}\n"
+		
+		printf "${BLUE}"
+		cat $CONFIG_FILE
+		printf "${END}"
+
+		echo
+	    echo "[1] Keep current"
+	    echo "[2] Create again"
+	    echo
+	    echo -n "Chose an option: "
+	    read optionConfig
+	    case $optionConfig in
+	        1) execCodeServer;;
+	        2) newConfigFile;;
+	        *) execCodeServer ;;
+	    esac
+	fi
+}
+
+newConfigFile(){
+	rm -Rf $CONFIG_FILE
+	touch $CONFIG_FILE
+
+	DEFAULT_HOST=127.0.0.1:8080
+
+	#bind-addr
+	clear
+	aboutConfigFile
+
+	printf "${GREEN}"
+	echo "VS Code Access Address!"
+	printf "${END}"
+	echo
+	echo "[1] Use default host address - http://"$DEFAULT_HOST
+	echo "[2] Define custom host"
+	echo
+	echo -n "Chose an option: "
+	read optionHost
+	case $optionHost in
+		1) echo "bind-addr: "$DEFAULT_HOST >> $CONFIG_FILE;;
+		2) echo "Enter the host and port in the format: domain.com:8181" && read bindAddr && echo "bind-addr:" $bindAddr >> $CONFIG_FILE;;
+		*) newConfigFile ;;
+	esac
+
+	#auth
+	echo
+	printf "${GREEN}"
+	echo "Authentication mode!"
+	printf "${END}"
+	echo
+	echo "[1] Generate random password"
+	echo "[2] Define custom password"
+	echo "[3] No password"
+	echo
+	echo -n "Chose an option: "
+	read optionAuth
+
+	NEWPWD=$(rand-str 15)
+	case $optionAuth in
+		1) echo -e "auth: password\npassword: "$NEWPWD >> $CONFIG_FILE;;
+		2) echo "Enter the custom password:" && read -s authPass && echo -e "auth: password\npassword:" $authPass >> $CONFIG_FILE;;
+		3) echo "auth: none" >> $CONFIG_FILE;;
+		*) newConfigFile ;;
+	esac
+
+	execCodeServer
+}
+
+execCodeServer(){
+	clear
+
+	printf "\n${GREEN}"
+	cat $CONFIG_FILE | grep password:
+	printf "${END}\n"
+	$CODE_FILE
+}
+
+fetch() {
+  URL="$1"
+  FILE="$2"
+
+  if [ -e "$FILE" ]; then
+    echoh "+ Reusing $FILE"
+    return
+  fi
+
+  sh_c curl \
+    -#fL \
+    -o "$FILE.incomplete" \
+    -C - \
+    "$URL"
+  sh_c mv "$FILE.incomplete" "$FILE"
+}
+
+
+sh_c() {
+  echoh "+ $*"
+  if [ ! "${DRY_RUN-}" ]; then
+    sh -c "$*"
+  fi
+}
+
+echoh() {
+  echo "$@" | humanpath
+}
+
+cath() {
+  humanpath
+}
+
+echoerr() {
+  echoh "$@" >&2
+}
+
+# humanpath replaces all occurances of " $HOME" with " ~"
+# and all occurances of '"$HOME' with the literal '"$HOME'.
+humanpath() {
+  sed "s# $HOME# ~#g; s#\"$HOME#\"\$HOME#g"
+}
+
+main "$@"
